79b7a0ec7c0709eb1ce65254b65155b3bc58c815
saif mulla <saif.mulla@strath.ac.uk>
2014-12-23 21:38:05 +0000

    complete twomolecule test with functionality

--- twomolecules/Twomolecules.C
+++ twomolecules/Twomolecules.C
@@ -27,6 +27,32 @@
 int main(int argc, char *argv[]){
     ::testing::InitGoogleTest(&argc,argv);
     
+    std::vector<double> expectPositions(24);
+    expectPositions[0] = 13.87527764;
+    expectPositions[1] = 7.86002214;
+    expectPositions[2] = 7.50948467;
+    expectPositions[3] = 14.08388005;
+    expectPositions[4] = 7.42784505;
+    expectPositions[5] = 7.51958947;
+    expectPositions[6] = 13.87527764;
+    expectPositions[7] = 7.59702001;
+    expectPositions[8] = 7.66092649;
+    expectPositions[9] = 13.90281172;
+    expectPositions[10] = 7.60939936;
+    expectPositions[11] = 7.62229790;
+    expectPositions[12] = 12.67240769;
+    expectPositions[13] = 7.40345187;
+    expectPositions[14] = 7.62258231;
+    expectPositions[15] = 12.59998852;
+    expectPositions[16] = 7.84172644;
+    expectPositions[17] = 7.44074453;
+    expectPositions[18] = 12.45678454;
+    expectPositions[19] = 7.61246793;
+    expectPositions[20] = 7.57872243;
+    expectPositions[21] = 12.50412739;
+    expectPositions[22] = 7.61513867;
+    expectPositions[23] = 7.56630471;
+    
     Foam::argList args(argc,argv);
     Foam::Time runTime(Foam::Time::controlDictName, args);
     Foam::fvMesh mesh
@@ -52,13 +78,17 @@ int main(int argc, char *argv[]){
     
     IDLList<Foam::polyMolecule>::iterator mol(molecules.begin());
     
+    int particles = 0;
     for (mol = molecules.begin(); mol != molecules.end(); ++mol){
-        int molsize = mol().siteForces().size();
+        int molsize = mol().sitePositions().size();
         int m = 0;
         while(m<molsize){
-            double tempx = mol().siteForces()[m].x();
+            EXPECT_NEAR(expectPositions[particles],mol().sitePositions()[m].x(),2.0);
+	    EXPECT_NEAR(expectPositions[particles+1],mol().sitePositions()[m].y(),2.0);
+	    EXPECT_NEAR(expectPositions[particles+2],mol().sitePositions()[m].z(),2.0);
             m++;
         }
+        particles+=3;
     }
     
     
